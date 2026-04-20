a493071ce0a65d2c40595fd67efe28b9e45fcae2
nerenererr <dinrss22@gmail>
2026-04-20 11:09:12 +0200

    mysql connection

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+        String url = "jdbc:mysql://127.0.0.1:3306/productora";
+        String user = "root";
+        String pass = "";
 
+        try (Connection conn = DriverManager.getConnection(url, user, pass)) {
+            System.out.println("Conexión establecida");
+        } catch (SQLException e) {
+            System.out.println("Error de conexión "
+            + e.getMessage());
+        }
     }
 }
